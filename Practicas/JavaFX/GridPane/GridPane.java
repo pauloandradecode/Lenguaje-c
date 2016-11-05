ab746cef2a25592b5a1befc3cfb43a01b8faf959
Paulo Andrade <source.compu@gmail.com>
2016-11-04 23:16:21 -0600

    Creando proyecto con gridpane en javafx

---
+++ Practicas/JavaFX/GridPane/GridPane.java
@@ -0,0 +1,43 @@
+/********************************************************
+Layput de tipo rejilla flexible
+
+Autor: Paulo Andrade
+Fecha: 04/11/2016
+********************************************************/
+
+// Importamos la libreria de javaFX
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+// Clase principal de la aplicacion
+public class GridPane extends Application
+{
+	// Punto principal del programa (Stage es el contenedor principal)
+	public void start(Stage primaryStage)
+	{
+		primaryStage.setTitle("JavaFX welcome"); // Titulo de la ventana
+		primaryStage.show(); // Mostramos la ventana
+
+		GridPane grid = new GridPane(); // Creamos una rejilla
+		grid.setAlignment(Pos.CENTER); // Posicion del grid
+		grid.setHgap(10); // Espacio entre filas
+		grid.setVgap(10); // Espacio entre columnas
+		grid.setPadding(new Insets(25, 25, 25, 25)); // Espacio del borde
+
+		Scene escena = new Scene(grid, 300, 275); // Nodo root
+		primaryStage.setScene(escena);
+	}
+
+	// Metodo principal
+	public static void main(String[] args)
+	{
+		// Iniciamos la aplicacion
+		launch(args);
+	}
+}
