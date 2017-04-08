893f6d063a97fedc0673b9456b8ce11e9820d4a6
Paulo andrade <source.compu@gmail.com>
2017-04-08 12:11:57 -0500

    Corrigiendo algoritmo.

--- Practicas/InterTecnologicos2016/UnDetected/undetected.cpp
+++ Practicas/InterTecnologicos2016/UnDetected/undetected.cpp
@@ -11,33 +11,35 @@ int main()
     int unsigned n, x, y, r;
     int count = 0; // contador
 
-    // Ingresamos el numero de sensores
-    do{
-        scanf("%d", &n);
-    } while (n > 200); // Pide el numero miestras sea mayor a 200
+    while(true){
+        // Ingresamos el numero de sensores
+        do{
+            scanf("%d", &n);
+        } while (n > 200); // Pide el numero miestras sea mayor a 200
 
-    // Recorremos todos los sensores
-    for(int i = 0; i < n; i++){
-        // Ingresamos las coordenadas y el radio
-        scanf("%d %d %d", &x, &y, &r);
+        // Recorremos todos los sensores
+        for(int i = 0; i < n; i++){
+            // Ingresamos las coordenadas y el radio
+            scanf("%d %d %d", &x, &y, &r);
 
-        // Calculamos para x y y mayor a la media
-        if(x > 100 && (x + r) <= 200){
-            if((y > 150 && (y + r) <= 300) || (y <= 150 && r <= y)){
-                count++;
+            // Calculamos para x y y mayor a la media
+            if(x > 100 && (x + r) <= 200){
+                if((y > 150 && (y + r) <= 300) || (y <= 150 && r <= y)){
+                    count++;
+                }
+            }
+
+            // Calculamos para x y y menor a la media
+            if(x <= 100 && r <= x){
+                if((y <= 150 && r <= y) || (y > 150 && (y + r) <= 300)){
+                    count++;
+                }
             }
         }
 
-        // Calculamos para x y y menor a la media
-        if(x <= 100 && r <= x){
-            if((y <= 150 && r <= y) || (y > 150 && (y + r) <= 300)){
-                count++;
-            }
-        }
+        // Mostramos el resultado
+        printf("%d\n\n", count);
     }
-
-    // Mostramos el resultado
-    printf("%d", count);
 
     return 0;
 }
