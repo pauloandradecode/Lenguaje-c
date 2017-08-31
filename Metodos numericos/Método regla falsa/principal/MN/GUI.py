52675841d939e1ea9f5c6c4e7548fd8543224996
Paulo andrade <source.compu@gmail.com>
2017-08-31 18:28:34 -0500

    Metodo de la regla falsa con bugs corregidos.

---
+++ Metodos numericos/Método regla falsa/principal/MN/GUI.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 30 00:43:06 2017
+
+@author: paulo
+"""
+
+import tkinter as tk
+import ttk
+import tkMessageBox
+from MN.ReglaFalsa import ReglaFalsa
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+
+class GUI(tk.Frame):
+    """ Interfaz grafica de usuario """
+
+    def __init__(self, parent=None):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+        self.graph = tk.Toplevel(self.parent)
+        self.initComponent()
+        self.initGraph()
+        self.graph.withdraw()
+        self.parent.mainloop()
+
+    def initComponent(self):
+        """ Componentes de la aplicación """
+        self.parent.title("Método de la regla falsa")
+        self.parent.resizable(0, 0)
+        self.parent.config(bg="white")
+        self.renderingWindow(self.parent)
+        
+        # Opciones
+        font = ('Verdana', 14)
+        bg = "white"
+        vcmd = (self.parent.register(self.validate),
+                '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
+        
+        # Objetos
+        lblEcuacion = tk.Label(self.parent, text="Ingrese la ecuación:",
+                               anchor=tk.W, justify=tk.LEFT)
+        lblEcuacion.place(x=10, y=10, width=300, height=20)
+        lblEcuacion.config(font=font, bg=bg)
+        
+        txtInstruccion = "Ingrese los limites del intervalo (Xi, XD):"
+        lblInstruccion = tk.Label(self.parent, text=txtInstruccion,
+                                  anchor=tk.W, justify=tk.LEFT)
+        lblInstruccion.place(x=10, y=50, width=700, height=20)
+        lblInstruccion.config(font=font, bg=bg)
+        
+        lblInferior = tk.Label(self.parent, text="L.I. Xi:",
+                                  anchor=tk.W, justify=tk.LEFT)
+        lblInferior.place(x=10, y=90, width=110, height=20)
+        lblInferior.config(font=font, bg=bg)
+        
+        lblSuperior = tk.Label(self.parent, text="L.S. Xd",
+                                  anchor=tk.W, justify=tk.LEFT)
+        lblSuperior.place(x=280, y=90, width=110, height=20)
+        lblSuperior.config(font=font, bg=bg)
+        
+        lblError = tk.Label(self.parent, text="Error:",
+                                  anchor=tk.W, justify=tk.LEFT)
+        lblError.place(x=540, y=90, width=110, height=20)
+        lblError.config(font=font, bg=bg)
+        
+        lblRes = tk.Label(self.parent, text="Resultados:",
+                                  anchor=tk.W, justify=tk.LEFT)
+        lblRes.place(x=10, y=170, width=700, height=20)
+        lblRes.config(font=font, bg=bg)
+        
+        self.txtEcuacion = tk.Entry(self.parent)
+        self.txtEcuacion.place(x=320, y=10, width=200, height=20)
+        self.txtEcuacion.focus()
+        
+        self.txtInferior = tk.Entry(self.parent)
+        self.txtInferior.place(x=130, y=90, width=130, height=20)
+        
+        self.txtSuperior = tk.Entry(self.parent)
+        self.txtSuperior.place(x=400, y=90, width=120, height=20)
+        
+        self.txtError = tk.Entry(self.parent, validate = 'key',
+                                 validatecommand = vcmd)
+        self.txtError.place(x=660, y=90, width=130, height=20)
+        
+        btnGrafica = tk.Button(self.parent, text="Graficar",
+                               command=lambda: self.graficar())
+        btnGrafica.place(x=670, y=10, width=120, height=20)
+        btnGrafica.config(font=font)
+        
+        btnRaiz = tk.Button(self.parent, text="Obtener raices",
+                            command=lambda: self.raiz())
+        btnRaiz.place(x=10, y=130, width=200, height=20)
+        btnRaiz.config(font=font)
+        
+        self.tree = ttk.Treeview(self.parent)
+        vsb = ttk.Scrollbar(self.parent, orient="vertical",
+                            command=self.tree.yview)
+        vsb.place(x=775, y=210, height=380)
+        self.tree.configure(yscrollcommand=vsb.set)
+        self.tree["columns"]=("[Xi, Xd]", "f(Xi)", "f(Xd)",
+                 "Xm", "f(Xm)")
+        self.tree.column("#0", width=50)
+        self.tree.column("[Xi, Xd]", width=125 )
+        self.tree.column("f(Xi)", width=180)
+        self.tree.column("f(Xd)", width=180)
+        self.tree.column("Xm", width=50)
+        self.tree.column("f(Xm)", width=180)
+        self.tree.heading("[Xi, Xd]", text="[Xi, Xd]")
+        self.tree.heading("f(Xi)", text="f(Xi)")
+        self.tree.heading("f(Xd)", text="F(Xd)")
+        self.tree.heading("Xm", text="Xm")
+        self.tree.heading("f(Xm)", text="f(Xm)")
+        self.tree.place(x=10, y=210, width=765, height=380)
+        self.tree.insert("" , 0, text="1", values=("","","","",""))
+        
+    def initGraph(self):
+        """ Componentes de la aplicación """
+        self.graph.title("Grafica")
+        self.graph.resizable(0, 0)
+        self.graph.config(bg="white")
+        self.renderingWindow(self.graph)
+        #self.graph.focus_set() # Focus
+        self.graph.protocol("WM_DELETE_WINDOW", "onexit") # quitamos cerrar
+        
+    def renderingWindow(self, frame):
+        """ Le da tamaño a la ventana y la centra en pantall """
+        w = 800
+        h = 600
+        ws = self.parent.winfo_screenwidth()
+        hs = self.parent.winfo_screenheight()
+        x = (ws/2) - (w/2)
+        y = (hs/2) - (h/2)
+        frame.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        
+    def raiz(self):
+        # Obtenemos los valores
+        ec = self.txtEcuacion.get()
+        xi = self.txtInferior.get()
+        xd = self.txtSuperior.get()
+        err = self.txtError.get()
+        text = "f(x) = "+str(ec)+"\n\n"
+        
+        # verificamos que todos los campos esten llenos
+        if len(ec)!=0 and len(xi)!=0 and len(xd)!=0 and len(err)!=0 :
+            # Limpiamos el treeView
+            self.tree.delete(*self.tree.get_children())
+            
+            # Creamos un objeto de la clase Regla Falsa
+            rf = ReglaFalsa(xi, xd, ec, err, self.tree)
+            
+            # Obtenemos el total de raices
+            rf.roots()
+            
+            if rf.root == 0 :
+                if len(rf.complex) == 0:
+                    # Mensaje a mostrar
+                    text = "La función dada no tiene raices"
+                    
+                    # Mostramos el mensaje
+                    tkMessageBox.showerror(title="Error", message=text)
+                else :
+                    # Contador de raices
+                    count = 1
+                    
+                    # Obtenemos las raices imaginarias
+                    for r in rf.complex:
+                        text += "Raiz "+str(count)+": ("+str(r[0])+", "+str(r[1])+" * i)\n"
+                        count += 1
+                        
+                    # Mostramos el mensaje
+                    tkMessageBox.showinfo(title="Resultados", message=text)
+            else :
+                # Intervalo inicial
+                intVal = rf.intervalo()
+                if intVal == 1 :
+                    
+                    # Obtenemos la raiz
+                    raiz = rf.estimate(1)
+                    
+                    # Agregamos informacion
+                    text += "Raíz "+str(1)+": "+str(raiz)+"\n"
+                    
+                    # Contador para raices
+                    count = 2
+                    
+                    # Obtenemos todas las raices reales
+                    while count <= rf.root:
+                        # Obtenemos la raiz
+                        raiz = rf.estimate(1)
+                        
+                        # Reordenamos el intervalo
+                        rf.xi = raiz + 1
+                        rf.xd = rf.xtemp
+                        
+                        # Agregamos informacion
+                        text += "Raíz "+str(count)+": "+str(raiz)+"\n"
+                        
+                        # Aumentamos el contador
+                        count += 1
+                    
+                    # Obtenemos las raices imaginarias
+                    for r in rf.complex:
+                        text += "Raiz "+str(count)+": ("+str(r[0])+", "+str(r[1])+" * i)\n"
+                        count += 1
+                    
+                    # Mostramos el mensaje
+                    tkMessageBox.showinfo(title="Resultados", message=text)
+                elif intVal == 2 :
+                    # Contador de raices
+                    count = 2
+                    
+                    # Agregamos informacion
+                    text += "Raíz "+str(count)+": 0\n"
+                    
+                    # Obtenemos las raices imaginarias
+                    for r in rf.complex:
+                        text += "Raiz "+str(count)+": ("+str(r[0])+", "+str(r[1])+" * i)\n"
+                        count += 1
+                    
+                    # Mostramos el mensaje
+                    tkMessageBox.showinfo(title="Resultados", message=text)
+                elif intVal == 3 :
+                    # Agregamos informacion
+                    text += "No se encontro una raiz en el intervalo dado"
+                    # Mostramos el mensaje
+                    tkMessageBox.showerror(title="Error", message=text)
+        else :
+            # Mensaje de error
+            text = "Por favor llene todos los campos"
+            # Mostramos el mensaje
+            tkMessageBox.showerror(title="Error", message=text)
+            
+    def validate(self, action, index, value_if_allowed,
+                       prior_value, text, validation_type, trigger_type, widget_name):
+        if action == "1":
+            if text in '0123456789.\-\+':
+                try:
+                    float(value_if_allowed)
+                    return True
+                except ValueError:
+                    return False
+            else:
+                return False
+        else:
+            return True
+        
+    def graficar(self):
+        """
+        Graficamos la funcion con ayuda de matplotlib
+        """
+        
+        font = ('Verdana', 14)
+        ec = self.txtEcuacion.get().lower()
+        
+        # verificamos que todos los campos esten llenos
+        if len(ec) != 0 :
+            # Limpiamos la ventana
+            for widget in self.graph.winfo_children():
+                widget.destroy()
+    
+            self.graph.deiconify()
+            rf = ReglaFalsa(0,0, ec)
+            
+            f = Figure(figsize=(5,5), dpi=100)
+            a = f.add_subplot(111)
+            # Valores del eje X que toma el gráfico.
+            _x = range(-10, 15)
+            # Graficar ambas funciones.
+            a.plot(_x, [rf.f(i) for i in _x])
+            # Establecer el color de los ejes.
+            a.axhline(0, color="black")
+            a.axvline(0, color="black")
+            # Limitar los valores de los ejes.
+            a.set_xlim(-10, 10)
+            a.set_ylim(-50, 50)
+            # Etiquetas de los ejes
+            a.set_xlabel('X')
+            a.set_ylabel('Y')
+            a.set_title('f(x) = '+str(ec))
+            a.grid(True)
+    
+            canvas = FigureCanvasTkAgg(f, self.graph)
+            canvas.show()
+            canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH,
+                                expand=True)
+    
+            toolbar = NavigationToolbar2TkAgg(canvas, self)
+            toolbar.update()
+            canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+            
+            btnClose = tk.Button(self.graph, text="Cerrar",
+                            command=lambda: self.close())
+            btnClose.pack(side=tk.BOTTOM, fill=tk.BOTH)
+            btnClose.config(font=font)
+        else:
+            # Mensaje de error
+            text = "Por favor ingrese la ecuacion"
+            tkMessageBox.showerror(title="Error", message=text)
+            
+    def close(self):
+        self.graph.withdraw();
---
+++ Metodos numericos/Método regla falsa/principal/MN/ReglaFalsa.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 29 22:32:25 2017
+
+@author: paulo
+Metodo de la regla falsa:
+    
+   Este método, como en el método de la bisección, parte de
+   dos puntos que rodean a la raíz f(x) = 0, es decir, dos
+   puntos x0 y x1 tales que f(x0)f(x1) < 0.
+   
+   La siguiente aproximación, x2, se calcula como la intersección
+   con el eje X de la recta que une ambos puntos (empleando la
+   ecuación (35) del método de la secante).
+"""
+
+import sympy as sp
+import cmath as cm
+
+class ReglaFalsa:
+    def __init__(self, xi = 0, xd = 0,
+                 ecuacion="x**3 + x**2 + 10", err = 0.001,
+                 treeView=None):
+        """
+        Constructor de la clase
+        """
+        
+        self.xi = sp.sympify(sp.Float(xi)) # limite inferior
+        self.xd = sp.sympify(sp.Float(xd)) # limite superior
+        self.xtemp = sp.sympify(sp.Float(xi)) # limite temporal
+        self.err = sp.sympify(sp.Float(err)) # margen de error
+        self.ec = str(ecuacion).lower() # ecuacion
+        self.root = 0 # raices racionales
+        self.complex = [] # raices complejas
+        self.tree = treeView
+        
+    def roots(self):
+        """
+        Obtenemos el total de raices de la ecuacion
+        """
+        
+        # Verificamos el grado del polinomio
+        x = sp.Symbol("x") # Declaramos x como simbolo
+        res = sp.solve(sp.sympify(self.ec), x) # obtenemos las raices
+
+        for i in res:
+            temp = cm.polar(sp.sympify(i)) # Obtenemos raiz en forma polar
+            
+            # Comprobamos si es compleja
+            if temp[0] == 0.0 and temp[1] == 0.0 :
+                self.root += 1
+            elif temp[0] != 0 and temp[1] == 0.0 :
+                self.root += 1
+            else :
+                self.complex.append(temp)
+    
+    def f(self, n):
+        """
+        f(x) a resolver
+        """
+        
+        x = sp.Symbol("x") # convertimos de variable a simbolo
+        ec = str(self.ec) # Convertimos en cadena
+        ec = sp.sympify(self.ec) # Convertimos a expresión
+        return ec.subs(x, n) #resolvemos
+    
+    def xm(self, xi, xd):
+        """
+        Formula para obtener la aproximacion de la raiz
+        """
+        
+        return (xd)-(((xd - xi)*self.f(xd)) / (self.f(xd)-self.f(xi)))
+    
+    def intervalo(self):
+        """
+        A partir de los valores dados, calculamos un intervalo inicial
+        """
+        
+        xr = (self.xi + self.xd) / 2 # Obtenemos el punto medio
+        fxi = self.f(self.xi) # funcion de xi
+        fxr = self.f(xr) # funcion de xr
+        
+        # Verificamos que el intervalo este bien definido
+        if self.xi < self.xd :
+            # Verificamos si hay cambio de signo
+            if fxi * fxr < 0:
+                # Si hay cambio de signo, la raiz se encuentra en este rango
+                self.xd = xr
+            elif fxi * fxr > 0:
+                # si no hay cambio de signo, recalculamos el intervalo
+                self.xi = xr
+                self.intervalo()
+            elif fxi * fxr == 0:
+                # Si es igual a 0, la raiz es 0
+                return 2
+        else :
+            # No tiene raices en ese intervalo
+            return 3
+        
+        return 1
+    
+    def estimate(self, count):
+        fxi = self.f(self.xi) # funcion de xi
+        fxd = self.f(self.xd) # funcion de xd
+        xm = self.xm(self.xi, self.xd) # obtenemos xm
+        fxm = self.f(xm) # funcion de xm
+        
+        # Mostramos la informacion
+        item = self.tree.insert("" , (count - 1), text=str(count), values=(
+                "["+str(sp.Float(self.xi, 5))+", "+str(sp.Float(self.xd, 5))+"]",
+                "f(" + str(sp.Float(self.xi, 8)) + ") = " + str(sp.Float(fxi, 8)),
+                "f(" + str(sp.Float(self.xd, 8)) + ") = " + str(sp.Float(fxd, 8)),
+                str(sp.Float(xm, 4)),
+                "f(" + str(sp.Float(xm, 8)) + ") = " + str(sp.Float(fxm, 8))
+            ))
+        
+        # Obtenemos el nuevo intervalo
+        if fxi < 0 :
+            self.xi = xm
+        if fxd < 0 :
+            self.xd = xm
+        
+        # Verificamos la raiz con el error estimado
+        count += 1 # Aumentamos el contador
+        self.tree.selection_set(item) # Seleccionamos la ultima fila
+        
+        # verificamos el error
+        if abs(fxm) > self.err : return self.estimate(count)
+        else: return xm
